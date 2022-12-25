2287d468c4347cfb64164e42ddce547da6aff1b2
Ítalo Rodrigo <italo.computation@gmail.com>
2022-12-25 18:10:36 -0400

    update myhomepage
    update myhomepage

--- lib/views/myhomepage.dart
+++ lib/views/myhomepage.dart
@@ -21,6 +21,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: AppBar(
         title: Text(widget.title),
       ),
